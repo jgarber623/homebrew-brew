c74367bef449041028d8c117ad553e70b0f1c053
Jason Garber <jason@sixtwothree.org>
2019-01-03 13:36:08 -0500

    Fix font paths

--- Casks/font-ia-writer-mono.rb
+++ Casks/font-ia-writer-mono.rb
@@ -6,10 +6,10 @@ cask 'font-ia-writer-mono' do
   name 'iA Writer Mono'
   homepage 'https://github.com/iaolo/iA-Fonts'
 
-  font 'iA-Fonts-master/iA Writer Duospace/Static/iAWriterMonoS-Bold.ttf'
-  font 'iA-Fonts-master/iA Writer Duospace/Static/iAWriterMonoS-BoldItalic.ttf'
-  font 'iA-Fonts-master/iA Writer Duospace/Static/iAWriterMonoS-Italic.ttf'
-  font 'iA-Fonts-master/iA Writer Duospace/Static/iAWriterMonoS-Regular.ttf'
-  font 'iA-Fonts-master/iA Writer Duospace/Variable/iAWriterMonoV-Italic.ttf'
-  font 'iA-Fonts-master/iA Writer Duospace/Variable/iAWriterMonoV.ttf'
+  font 'iA-Fonts-master/iA Writer Mono/Static/iAWriterMonoS-Bold.ttf'
+  font 'iA-Fonts-master/iA Writer Mono/Static/iAWriterMonoS-BoldItalic.ttf'
+  font 'iA-Fonts-master/iA Writer Mono/Static/iAWriterMonoS-Italic.ttf'
+  font 'iA-Fonts-master/iA Writer Mono/Static/iAWriterMonoS-Regular.ttf'
+  font 'iA-Fonts-master/iA Writer Mono/Variable/iAWriterMonoV-Italic.ttf'
+  font 'iA-Fonts-master/iA Writer Mono/Variable/iAWriterMonoV.ttf'
 end
