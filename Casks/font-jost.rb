aabb2d1045cdd0da90ee441faa62b9d0858bbd16
Jason Garber <jason@sixtwothree.org>
2019-02-06 23:22:40 -0500

    Add Bodoni* and Jost* casks

---
+++ Casks/font-jost.rb
@@ -0,0 +1,28 @@
+cask 'font-jost' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://codeload.github.com/indestructible-type/Jost/zip/master'
+  name 'Jost*'
+  homepage 'https://github.com/indestructible-type/Jost'
+
+  font 'Jost-master/Finished OpenType/Jost-100-Hairline.otf'
+  font 'Jost-master/Finished OpenType/Jost-100-HairlineItalic.otf'
+  font 'Jost-master/Finished OpenType/Jost-200-Thin.otf'
+  font 'Jost-master/Finished OpenType/Jost-200-ThinItalic.otf'
+  font 'Jost-master/Finished OpenType/Jost-300-Light.otf'
+  font 'Jost-master/Finished OpenType/Jost-300-LightItalic.otf'
+  font 'Jost-master/Finished OpenType/Jost-400-Book.otf'
+  font 'Jost-master/Finished OpenType/Jost-400-BookItalic.otf'
+  font 'Jost-master/Finished OpenType/Jost-500-Medium.otf'
+  font 'Jost-master/Finished OpenType/Jost-500-MediumItalic.otf'
+  font 'Jost-master/Finished OpenType/Jost-600-Semi.otf'
+  font 'Jost-master/Finished OpenType/Jost-600-SemiItalic.otf'
+  font 'Jost-master/Finished OpenType/Jost-700-Bold.otf'
+  font 'Jost-master/Finished OpenType/Jost-700-BoldItalic.otf'
+  font 'Jost-master/Finished OpenType/Jost-800-Heavy.otf'
+  font 'Jost-master/Finished OpenType/Jost-800-HeavyItalic.otf'
+  font 'Jost-master/Finished OpenType/Jost-900-Black.otf'
+  font 'Jost-master/Finished OpenType/Jost-900-BlackItalic.otf'
+
+end
