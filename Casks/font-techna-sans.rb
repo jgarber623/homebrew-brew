f5b5114888ead01039fd1edd052b0c4cfe939ade
Jason Garber <jason@sixtwothree.org>
2019-06-29 09:27:50 -0400

    Add Techna Sans font

---
+++ Casks/font-techna-sans.rb
@@ -0,0 +1,18 @@
+cask 'font-techna-sans' do
+  version '1.003'
+  sha256 'd5526bf40110c95efbfd465b6a4f6913e7255928f23e978534b687d99a22c6f4'
+
+  url "https://github.com/carlenlund/techna-sans/releases/download/#{version}/techna-sans-#{version}.zip"
+  appcast 'https://github.com/carlenlund/techna-sans/releases.atom'
+  name 'Techna Sans'
+  homepage 'https://github.com/carlenlund/techna-sans'
+
+  font 'techna-sans/TechnaSans-Regular.otf'
+
+  caveats <<~EOS
+    Copyright (c) 2019, Carl Enlund
+
+    This Font Software is licensed under the SIL Open Font License, Version 1.1.
+    This license is available with a FAQ at: http://scripts.sil.org/OFL
+  EOS
+end
