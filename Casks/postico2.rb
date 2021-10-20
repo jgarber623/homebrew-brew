b5848d4d4a1f62c91d7b2e48caca304c0ccf5f21
Jason Garber <jason@sixtwothree.org>
2021-10-20 15:02:59 -0400

    Add postico2 cask

---
+++ Casks/postico2.rb
@@ -0,0 +1,10 @@
+cask 'postico2' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://releases.postico.app/v2/download'
+  name 'Postico 2'
+  homepage 'https://eggerapps.at/postico2/'
+
+  app 'Postico 2.app'
+end
