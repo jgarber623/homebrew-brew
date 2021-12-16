0c91868ecc88d7e09f6b5dbaaa2e05a964b6b7b6
Jason Garber <jason@sixtwothree.org>
2021-12-16 16:47:00 -0500

    Audit/style fix casks

--- Casks/font-techna-sans.rb
+++ Casks/font-techna-sans.rb
@@ -1,13 +1,13 @@
-cask 'font-techna-sans' do
-  version '1.003'
-  sha256 'd5526bf40110c95efbfd465b6a4f6913e7255928f23e978534b687d99a22c6f4'
+cask "font-techna-sans" do
+  version "1.003"
+  sha256 "d5526bf40110c95efbfd465b6a4f6913e7255928f23e978534b687d99a22c6f4"
 
   url "https://github.com/carlenlund/techna-sans/releases/download/#{version}/techna-sans-#{version}.zip"
-  appcast 'https://github.com/carlenlund/techna-sans/releases.atom'
-  name 'Techna Sans'
-  homepage 'https://github.com/carlenlund/techna-sans'
+  name "Techna Sans"
+  desc "Bold, semi-geometric sans-serif typeface"
+  homepage "https://github.com/carlenlund/techna-sans"
 
-  font 'techna-sans/TechnaSans-Regular.otf'
+  font "techna-sans/TechnaSans-Regular.otf"
 
   caveats <<~EOS
     Copyright (c) 2019, Carl Enlund
--- Casks/postico2.rb
+++ Casks/postico2.rb
@@ -1,10 +1,11 @@
-cask 'postico2' do
+cask "postico2" do
   version :latest
   sha256 :no_check
 
-  url 'https://releases.postico.app/v2/download'
-  name 'Postico 2'
-  homepage 'https://eggerapps.at/postico2/'
+  url "https://releases.postico.app/v2/download", verified: "releases.postico.app"
+  name "Postico 2"
+  desc "Native Mac app for PostgreSQL"
+  homepage "https://eggerapps.at/postico2/"
 
-  app 'Postico 2.app'
+  app "Postico 2.app"
 end
