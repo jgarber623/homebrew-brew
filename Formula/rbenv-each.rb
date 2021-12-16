508474710bbfaacd7d6f7cecae69b6ade456da9f
Jason Garber <jason@sixtwothree.org>
2021-12-16 16:50:02 -0500

    Use tar.gz instead of zip

--- Formula/rbenv-each.rb
+++ Formula/rbenv-each.rb
@@ -1,7 +1,7 @@
 class RbenvEach < Formula
   desc "Run a command across all installed rubies"
   homepage "https://github.com/rbenv/rbenv-each"
-  url "https://github.com/rbenv/rbenv-each/archive/1e5dcdc0a0daa3a48971c604086ed60b67632063.zip"
+  url "https://github.com/rbenv/rbenv-each/archive/1e5dcdc0a0daa3a48971c604086ed60b67632063.tar.gz"
   sha256 "60e21184288a587e58ad260c50dd803d5bb5e30741092705dfdf148447fd69a4"
 
   depends_on "rbenv"
