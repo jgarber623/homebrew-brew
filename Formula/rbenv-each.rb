9f307ad84511acee5325fc23f98c00554d9a97bd
Jason Garber <jason@sixtwothree.org>
2022-05-16 14:56:07 -0400

    Update rbenv-each shasum

--- Formula/rbenv-each.rb
+++ Formula/rbenv-each.rb
@@ -2,7 +2,7 @@ class RbenvEach < Formula
   desc "Run a command across all installed rubies"
   homepage "https://github.com/rbenv/rbenv-each"
   url "https://github.com/rbenv/rbenv-each/archive/1e5dcdc0a0daa3a48971c604086ed60b67632063.tar.gz"
-  sha256 "60e21184288a587e58ad260c50dd803d5bb5e30741092705dfdf148447fd69a4"
+  sha256 "f022f7384d291eb85cd5d9adf792b2c387a9304f24b123cd9d264ac212f6c253"
 
   depends_on "rbenv"
 
