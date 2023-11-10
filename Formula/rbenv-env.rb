a82c907d9df9898ea65fb43d9f84e02ef2285fe8
Jason Garber <jason@sixtwothree.org>
2023-11-10 15:03:57 -0500

    Prefer "archive/refs/tags" URLs

--- Formula/rbenv-env.rb
+++ Formula/rbenv-env.rb
@@ -1,7 +1,7 @@
 class RbenvEnv < Formula
   desc "Shows relevant environment variables"
   homepage "https://github.com/ianheggie/rbenv-env"
-  url "https://github.com/ianheggie/rbenv-env/archive/1.4.tar.gz"
+  url "https://github.com/ianheggie/rbenv-env/archive/refs/tags/1.4.tar.gz"
   sha256 "5acb14cbd63babcd19cdc9355c8e9bbd30374750281371108e1edacfdeceaa97"
 
   depends_on "rbenv"
--- Formula/rbenv-gem-update.rb
+++ Formula/rbenv-gem-update.rb
@@ -1,7 +1,7 @@
 class RbenvGemUpdate < Formula
   desc "Automatically run `gem update --system` whenever you `rbenv install` a Ruby"
   homepage "https://github.com/nicknovitski/rbenv-gem-update"
-  url "https://github.com/nicknovitski/rbenv-gem-update/archive/v1.0.0.tar.gz"
+  url "https://github.com/nicknovitski/rbenv-gem-update/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "b80d4334aa9cf7bbafe17a829deec66f44951d3a7f5df38486becc09a38e4363"
 
   depends_on "rbenv"
