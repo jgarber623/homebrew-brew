1b910f11f08b8e2293b2adfd067f56dab9d81c9c
Jason Garber <jason@sixtwothree.org>
2024-02-12 16:41:30 -0500

    Add Tapestry Loom (beta) cask

---
+++ Casks/tapestry-loom.rb
@@ -0,0 +1,11 @@
+cask "tapestry-loom" do
+  version :latest
+  sha256 :no_check
+
+  url "https://files.iconfactory.net/software/beta/Tapestry-Loom.zip"
+  name "Tapestry Loom"
+  desc "Weave your favorite blogs, social media, and more into a unified and chronological timeline"
+  homepage "https://github.com/TheIconfactory/Tapestry"
+
+  app "Tapestry Loom.app"
+end
